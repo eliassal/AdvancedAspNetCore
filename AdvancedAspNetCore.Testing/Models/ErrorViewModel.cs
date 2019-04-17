b6815cf38bacea407cb697b3282a55f0300e7b20
Salam Y. ELIAS <salam.elias@free.fr>
2019-04-17 11:25:10 +0200

    2nd commit

---
+++ AdvancedAspNetCore.Testing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdvancedAspNetCore.Testing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AdvancedAspNetCore.Tests/HomeControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using AdvancedAspNetCore.Testing.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvancedAspNetCore.Tests
+{
+    public class HomeControllerTests
+    {
+        public HomeControllerTests()
+        {
+
+        }
+
+        [Fact(DisplayName = "Index should return default view")]
+        public void Index_should_return_default_view()
+        {
+            var controller = new HomeController();
+            var viewResult = (ViewResult)controller.Index();
+            var viewName = viewResult.ViewName;
+            Assert.True(string.IsNullOrEmpty(viewName) || viewName == "Index");
+        }
+
+        [Fact(DisplayName = "Failing test")]
+        public void ShouldFail()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
